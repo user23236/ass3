a60b3d2b444528282116adcd1c06d6be404a3ff1
user23236 <kari.kuandyk11@gmail.com>
2026-05-01 21:22:49 +0500

    init commit

---
+++ src/Experiment.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Experiment {
+    private Sorter sorter;
+    private Searcher searcher;
+
+    public Experiment(Sorter sorter, Searcher searcher) {
+        this.sorter = sorter;
+        this.searcher = searcher;
+    }
+
+    public long measureSortTime(int[] arr, String type) {
+        int[] copy = Arrays.copyOf(arr, arr.length);
+
+        long start = System.nanoTime();
+
+        if (type.equalsIgnoreCase("basic")) {
+            sorter.basicSort(copy);
+        } else if (type.equalsIgnoreCase("advanced")) {
+            sorter.advancedSort(copy);
+        }
+
+        long end = System.nanoTime();
+        return end - start;
+    }
+
+    public long measureSearchTime(int[] arr, int target) {
+        long start = System.nanoTime();
+        searcher.search(arr, target);
+        long end = System.nanoTime();
+
+        return end - start;
+    }
+
+    public void runAllExperiments() {
+        int[] sizes = {10, 100, 1000};
+
+        for (int size : sizes) {
+            System.out.println("\n===== Array Size: " + size + " =====");
+
+            int[] randomArray = sorter.generateRandomArray(size);
+            int[] sortedArray = Arrays.copyOf(randomArray, randomArray.length);
+            Arrays.sort(sortedArray);
+
+            // Sorting Experiments
+            long basicRandom = measureSortTime(randomArray, "basic");
+            long advancedRandom = measureSortTime(randomArray, "advanced");
+
+            long basicSorted = measureSortTime(sortedArray, "basic");
+            long advancedSorted = measureSortTime(sortedArray, "advanced");
+
+            // Searching Experiment
+            int target = sortedArray[size / 2];
+            long searchTime = measureSearchTime(sortedArray, target);
+
+            System.out.println("Random Array:");
+            System.out.println("Basic Sort Time: " + basicRandom + " ns");
+            System.out.println("Advanced Sort Time: " + advancedRandom + " ns");
+
+            System.out.println("Sorted Array:");
+            System.out.println("Basic Sort Time: " + basicSorted + " ns");
+            System.out.println("Advanced Sort Time: " + advancedSorted + " ns");
+
+            System.out.println("Search Time (target " + target + "): " + searchTime + " ns");
+        }
+    }
+}
---
+++ src/Sorter.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+public class Sorter {
+
+    // Basic Sort: Insertion Sort
+    public void basicSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move larger elements one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    // Advanced Sort: Merge Sort
+    public void advancedSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    public void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(10000);
+        }
+
+        return arr;
+    }
+}
